524c2b96716de753ad1fccc5fe15149b41a65128
William Onyejiaka <williamonyejiaka08062528003@gmail.com>
2026-06-10 16:16:13 +0100

    fix: calculate file size using buffer byte length instead of file object property

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
   res.json({
     name: req.file.originalname,
     type: req.file.mimetype,
-    size: req.file.size
+    size: Buffer.byteLength(req.file.buffer).toString()
   });
 });
 
